ad5e08e23b304e14a8ac2906f8e5a03379fd9bd4
Andres Montoya Angulo <jmontoyaan@unsa.edu.pe>
2022-03-18 11:50:20 -0500

    Gran inversión

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './Componentes/PersonCard/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor={"Black"}/>
+      <PersonCard firstName="John" lastName="Smith" age={88} hairColor={"Browm"}/>
+      <PersonCard firstName="Millard" lastName="Fillmore" age={50} hairColor={"Browm"}/>
+      <PersonCard firstName="Maria" lastName="Smith" age={62} hairColor={"Browm"}/>
     </div>
   );
 }
